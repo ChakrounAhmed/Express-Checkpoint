37c836a0452827d7969e0790efe278f39f11f326
Ahmed CHAKROUN <102046411+ChakrounAhmed@users.noreply.github.com>
2023-07-12 15:04:32 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 5000;
-app.set("view engine", "pug");
-app.set("views", "./views");
+
 
 // Set up middleware for verifying working hours
 const authMiddleware = (req, res, next) => {
